794b20e7b9404f5facf089654c2ddd1341059e75
adriandelarco <adriandelarco@gmail.com>
2019-03-11 12:07:48 +0100

    Update extractors_desktop.js

--- src/extractors_desktop.js
+++ src/extractors_desktop.js
@@ -2,6 +2,10 @@ const { ensureItsAbsoluteUrl } = require('./tools');
 
 exports.extractOgranicResults = ($) => {
     const searchResults = [];
+    console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+    console.log('$('body')');
+    console.log('$('body').text()');
+    console.log('$('body').innerText');
     if  ((!$('body').text().match(/No se ha encontrado/)) && (!$('body').text().innerText.match(/No results found/))) {
         $('.g .rc').each((index, el) => {
             const siteLinks = [];
